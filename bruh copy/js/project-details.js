ba75d90cd93641652b9e41b1f52232918c76ec22
annarippert <anna.rippert@gmail.com>
2022-11-29 23:35:07 -0500

    updating work progress backup

---
+++ bruh copy/js/project-details.js
@@ -0,0 +1,9 @@
+// as the user scrolls, the progress bar moves to indicate the position on the page
+window.onscroll = function() {progressBar()};
+
+function progressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
