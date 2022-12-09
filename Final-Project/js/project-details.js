0fc89b5e4e666055343bcf37020aff083e0c52db
annarippert <anna.rippert@gmail.com>
2022-12-09 15:02:53 -0500

    fixing Wave error and adding scroll to top button

--- Final-Project/js/project-details.js
+++ Final-Project/js/project-details.js
@@ -1,3 +1,4 @@
+//---------------PROGRESS BAR--------------------
 // as the user scrolls, the progress bar moves to indicate the position on the page
 window.onscroll = function() {progressBar()};
 
@@ -8,6 +9,7 @@ function progressBar() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+//---------------JS LIBRARY SCROLL REVEAL--------------------
 // inspired by https://scrollrevealjs.org/guide/user-experience.html
 // using JS library ScrollReveal 
 
@@ -19,3 +21,27 @@ ScrollReveal().reveal('.project-findings-container', { delay: 500 })
 ScrollReveal().reveal('.project-solution', { delay: 500 })
 ScrollReveal().reveal('.project-other', { delay: 500 })
 ScrollReveal().reveal('.about_table', { delay: 500 })
+
+
+//---------------SCROLL TO TOP FUNCTION--------------------
+//inspired by https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// first call the button
+let goToTop = document.getElementById("backUpbutton");
+
+// button shows when user scrolls down
+window.onscroll = function() {ScrollToTop()};
+
+// functuon that checks where the user is with the scroll
+function ScrollToTop() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goToTop.style.display = "block";
+  } else {
+    goToTop.style.display = "none";
+  }
+}
+
+// if the button is clicked, they will be redirected to the top
+function scrollUp() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
